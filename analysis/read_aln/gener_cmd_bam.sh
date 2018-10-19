25fdd479b0f43a6eb441ae555749960e6a769d02
baoyi33 <44072697+baoyi33@users.noreply.github.com>
2018-10-19 14:13:53 -0500

    Delete gener_cmd_bam.sh

--- analysis/read_aln/gener_cmd_bam.sh
+++
@@ -1,5 +0,0 @@
-hisat2 -x ../../data/ref/yeast_ref -U ../../data/raw/ | samtools view -bS - | samtools sort -o EV_3.srt.bam
-hisat2 -x ../../data/ref/yeast_ref -U ../../data/raw/ | samtools view -bS - | samtools sort -o EV_4.srt.bam
-hisat2 -x ../../data/ref/yeast_ref -U ../../data/raw/ | samtools view -bS - | samtools sort -o DNMT3B_2.srt.bam
-hisat2 -x ../../data/ref/yeast_ref -U ../../data/raw/ | samtools view -bS - | samtools sort -o DNMT3B_3.srt.bam
-hisat2 -x ../../data/ref/yeast_ref -U ../../data/raw/ | samtools view -bS - | samtools sort -o DNMT3B_4.srt.bam
